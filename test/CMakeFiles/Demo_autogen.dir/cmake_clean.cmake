0ed385c800b0e8a0888a0313db88492a6f8ffa0e
Ekkosun <178380101@qq.com>
2020-12-10 13:48:55 +0800

    delete test

--- test/CMakeFiles/Demo_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Demo_autogen"
-  "Demo_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Demo_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
